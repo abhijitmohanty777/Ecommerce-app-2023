4fe0b43269de3929a5f36688eab66f49ec251ef5
abhijit <abhijit.mohanty@faarms.com>
2023-08-24 23:54:36 +0530

    added useGlobalInterceptors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { TransformationInterceptor } from './common/responseInterceptor';
 
 async function bootstrap() {
   const port = process.env.PORT;
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalInterceptors(new TransformationInterceptor)
+
   await app.listen(port, () => {
     console.log(`server is running on port: ${port}`);
   });
